f6e82b442ff518a8a41b3c599db177549f0e9cf0
JefferyBurgett <jeffery.burgett@gmail.com>
2022-10-02 17:41:51 -0500

    Created Music Midel in App + Registered Music and Rest Framework in Project Setting Installed Apps Section

---
+++ music/models.py
@@ -0,0 +1,12 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
+from MySQLdb import Date
+from django.db import models
+from datetime import datetime, date
+# Create your models here.
+class Music(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.DateField(auto_now=False, auto_now_add=False, default= 'YYYY-MM-DD')
+    genre = models.CharField(max_length=255)
