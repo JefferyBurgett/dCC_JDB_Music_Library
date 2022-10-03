9018f042a986d00d40f5cbd8b8f266340dc26437
JefferyBurgett <jeffery.burgett@gmail.com>
2022-10-02 22:52:27 -0500

    Created Update Functionality in APP Views File

--- music/views.py
+++ music/views.py
@@ -18,10 +18,15 @@ def music_list(request):
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def music_detail(request, pk):
         music = get_object_or_404(Music, pk=pk) 
         if request.method == 'GET':
             music = get_object_or_404(Music, pk=pk)
             serializer = MusicSerializer(music)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = MusicSerializer(music, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
             return Response(serializer.data)
